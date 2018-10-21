b9e47b4e5eabdb4fd00d1b4e5baa09a233f29249
dhani yanuar <dhani.y7@gmail.com>
2018-10-22 00:13:57 +0700

    Update Camerra Follow dan Spawner

---
+++ Planets/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform cloud;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;    
+    }
+
+
+    void Update()
+    {
+        if(waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for(int i=0; i<_wave.count; i++)
+        {
+            SpawnCloud(_wave.cloud);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+
+        state = SpawnState.WAITING;
+
+
+        yield break;
+    }
+
+
+    void SpawnCloud(Transform _cloud)
+    {
+        //spawn cloud;
+        Debug.Log("Spawning Enemy: " + _cloud.name);
+    }
+}
